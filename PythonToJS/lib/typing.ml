c8a15842301a23ff19e7e8596d306adf8371830b
yannisGi12 <yannisgilbert5@gmail.com>
2026-04-17 14:37:27 +0200

    Update typing.ml

--- PythonToJS/lib/typing.ml
+++ PythonToJS/lib/typing.ml
@@ -24,29 +24,64 @@ type environment = {
     }
   [@@deriving show]
 
-
-let rec tp_stmt ((env, t, returned) : (environment * tp * bool)) s = match s with 
-              | Block [Assign(v,e)] -> let t = tp_expr env e in Printf.printf "Type: %s\n"(Lang.show_tp t);true
-              |_ -> Printf.printf"type inconnu \n";true
-
-(* expr :
+(*type expr =
   Const of value                           (* constant *)
   | VarE of vname                            (* variable *)
   | BinOp of  binop * expr * expr            (* binary operation *)
   | CallE of vname * (expr list)             (* function call *)
   [@@deriving show]
   
-   value :
+type value =
   BoolV of bool
   | IntV of int
   | FloatV of float
   | NoneV
-  | StringV of string*)
+  | StringV of string
+  
+  type stmt =
+    Block of stmt list
+  | Assign of vname * expr                   (* Variable assignment: x = e *)
+  | Cond of expr * stmt * stmt               (* if .. then .. else *)
+  | While of expr * stmt                  
+  | Return of  expr
+  | CallS of vname * (expr list)             (* procedure call *)
+  [@@deriving show]
+  *)
 
-let tp_const v = match v with |BoolV(b) -> BoolT | IntV(i) -> IntT | FloatV(f) -> FloatT | StringV(s) -> StringT
+
+let tp_const v = match v with |BoolV(b) -> BoolT | IntV(i) -> IntT | FloatV(f) -> FloatT | StringV(s) -> StringT | NoneV -> NoneT ;;
+
+type 'a option =
+None
+| Some of 'a;;
+
+let rec lookg (env : var_environment) (var : vname) = match env.globals with 
+          |[] -> None
+          |(c,v)::l -> if c = var then (Some var) else look env var;;
+
+let rec lookl (env : var_environment) (var : vname) = match env.locals with 
+          |[] -> None
+          |(c,v)::l -> if c = var then (Some var) else look env var;;
+
+exception Variable_inexistante;;
 
 let rec tp_expr (env : environment) (exp : expr) : tp = match exp with
               | Const (v) -> tp_const(v)
+              | VarE (v) -> let looking = (lookg s env.dyn_vars) in (match looking with
+	                                        |None -> raise Variable_inexistante 
+	                                        |Some t -> t )
+              |BinOp -> 
+              |CallE -> 
+
+let rec tp_stmt ((env, t, returned) : (environment * tp * bool)) s = match s with 
+              | Block [Assign(v,e)] -> let t = tp_expr env e in Printf.printf "Type: %s\n"(Lang.show_tp t);true
+              |_ -> Printf.printf"type inconnu \n";true
+
+
+              
+
+
+
 
 let tp_fundefn init_env (Fundefn(Fundecl(fn, pards, rt), vds, s)) = true
 
