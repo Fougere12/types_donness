589ade7430b46802c67bd9de92f1004b023b2b4e
RobinSievic <sievicrobin@gmail.com>
2026-05-03 20:31:18 +0200

    Update pprinter.ml
    ajout de doc_of_value pour gérer les types de bases
    ajout de doc_of_binop pour gérer les différentes opérations
    ajout de doc_of_expr pour gérer les différents types d'expressions possibles
    modification de doc_of_stmt pour gérer le print -> alert
    modification de doc_of_fundefn pour gérer les fonctions

--- PythonToJS/lib/pprinter.ml
+++ PythonToJS/lib/pprinter.ml
@@ -7,26 +7,71 @@ let indent_level = 4
 
 let doc_of_var v = string v
 
-let doc_of_expr e = string "xxx"
+let doc_of_value v =
+  match v with
+  | BoolV b -> string (string_of_bool b)
+  | IntV i -> string (string_of_int i)
+  | FloatV f -> string (string_of_float f)
+  | NoneV -> string "null"
+  | StringV s -> string ("\"" ^ s ^ "\"")
+
+let doc_of_binop op =
+  match op with
+  | BArith BAadd -> string "+"
+  | BArith BAsub -> string "-"
+  | BArith BAmul -> string "*"
+  | BArith BAdiv -> string "/"
+  | BArith BAmod -> string "%"
+  | BBool BBand -> string "&&"
+  | BBool BBor -> string "||"
+  | BCompar BCeq -> string "=="
+  | BCompar BCge -> string ">="
+  | BCompar BCgt -> string ">"
+  | BCompar BCle -> string "<="
+  | BCompar BClt -> string "<"
+  | BCompar BCne -> string "!="
+
+let rec doc_of_expr e =
+  match e with
+  | Const x -> doc_of_value x
+  | VarE v -> doc_of_var v
+  | BinOp (op, e1, e2) -> parens (doc_of_expr e1 ^^ space ^^ doc_of_binop op ^^ space ^^ doc_of_expr e2)
+  | CallE (f, args) -> string f ^^ parens (separate_map comma doc_of_expr args)
 
 let doc_of_var_list vs = parens (separate_map comma string vs)
 
-(*let rec doc_of_stmt s = string "INCOMPLET"*)
+
 let rec doc_of_stmt s = match s with 
-  | Block (s::l) -> doc_of_stmt s 
+  | Block sl -> nest indent_level (separate hardline (List.map doc_of_stmt sl))
   | Assign(v, e) -> doc_of_var v ^^ space ^^ string "=" ^^ space ^^ doc_of_expr e ^^ string ";"
-  | Cond(e,s1,s2) -> string "if (" ^^ doc_of_expr e ^^ string ") {" ^^ doc_of_stmt s1 ^^ string "} else {" ^^ doc_of_stmt s2 ^^ string "}"
-  | While(e,s) -> string "while (" ^^ doc_of_expr e ^^ string ") {" ^^ doc_of_stmt s ^^ string "}"
+  | Cond(e,s1,s2) ->
+    string "if (" ^^ doc_of_expr e ^^ string ") {" ^^ hardline ^^
+    nest indent_level (doc_of_stmt s1) ^^ hardline ^^
+    string "} else {" ^^ hardline ^^
+    nest indent_level (doc_of_stmt s2) ^^ hardline ^^
+    string "}"
+  | While(e,s) ->
+    string "while (" ^^ doc_of_expr e ^^ string ") {" ^^ hardline ^^
+    nest indent_level (doc_of_stmt s) ^^ hardline ^^
+    string "}"
   | Return(e) -> string "return " ^^ doc_of_expr e ^^ string ";"
+  | CallS ("print", args) -> string "alert" ^^ parens (separate_map comma doc_of_expr args) ^^ string ";"
+  | CallS (f, args) -> string f ^^ parens (separate_map comma doc_of_expr args) ^^ string ";"
  
 
 let doc_of_local_vardecl (Vardecl(vn,_t)) = string "let" ^^ space ^^ string vn ^^ string ";"
-let doc_of_global_vardecl (Vardecl(vn,_t)) = string "var" ^^ space ^^ string vn ^^ string ";"
+let doc_of_global_vardecl (Vardecl(vn,_t)) = string "let" ^^ space ^^ string vn ^^ string ";"
+
 
 let doc_of_fundefn (Fundefn(Fundecl(fn, params, _rt), vds, s)) =
-      (separate space [
-        string "function"; string fn; (doc_of_var_list (List.map name_of_vardecl params))
-        ]) ^^ space ^^ string "INCOMPLET"
+  string "function" ^^ space ^^ string fn ^^
+  doc_of_var_list (List.map name_of_vardecl params) ^^ space ^^
+  string "{" ^^ hardline ^^
+  nest indent_level (
+    separate hardline (List.map doc_of_local_vardecl vds) ^^ hardline ^^
+    doc_of_stmt s
+  ) ^^ hardline ^^
+  string "}"
 
 let doc_of_prog (Prog(fdfs, vds, s)) = 
   (separate_map hardline doc_of_fundefn fdfs) ^^
