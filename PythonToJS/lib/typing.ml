9b85f713eb9cc55af48d07849e8ad2c8c9bc08cc
RobinSievic <sievicrobin@gmail.com>
2026-05-04 01:24:48 +0200

    Update typing.ml

--- PythonToJS/lib/typing.ml
+++ PythonToJS/lib/typing.ml
@@ -109,9 +109,9 @@ let rec tp_stmt ((env, t, returned) : (environment * tp * bool)) stm : (environm
 	                                                |None -> raise Variable_inexistante 
 	                                                |Some t -> t ))
 	                                        |Some t -> t )  
-                                        in if inclu (tp_expr ex) (type) then let new_dyn_vars = match look env.static_vars.locals v with
-                                                  | Some _ -> {env.dyn_vars.locals with locals = (v,type_v) :: env.dyn_vars.locals
-                                                  | None -> {env.dyn_vars.locals with globals = (v,type_v) :: env.dyn_vars.globals
+                                        in if inclu (tp_expr ex) (type) then let new_dyn_vars = match env.static_vars.locals v with
+                                                  | Some _ -> {env.dyn_vars.locals with locals = (v,type_v) :: env.dyn_vars.locals}
+                                                  | None -> {env.dyn_vars.globals with globals = (v,type_v) :: env.dyn_vars.globals}
                                               in let new_env = { env with dyn_vars = new_dyn_vars } in (new_env, t, false)
 											  else raise Variable_pas_instancie
               |
