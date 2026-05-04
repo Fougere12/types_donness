0352981b47626a18a5dbd30f164bcf289dd01a46
Fougere12 <ananasamendine@gmail.com>
2026-05-04 18:05:59 +0200

    Update evenodd.py

--- PythonToJS/test/evenodd.py
+++ PythonToJS/test/evenodd.py
@@ -10,6 +10,38 @@ def is_odd(n: int) -> bool:
     return((n != 0) and is_even(n - 1))
     #end
 
-x: int
-x = int(input("Enter value x: "))
-print("Is the result even? " + str(is_even(x)))
+def is_even(n: int) -> int:
+    #begin
+    y : int
+    y=3
+    n=2
+    n = n+1
+    if n==3:
+        #begin
+        return n-1
+        #end
+    else :
+        #begin
+        return n+4
+        #end
+    #end
+
+x : int
+x = 3
+if x==3:
+    #begin
+    x=4
+    x = 5
+    #end
+else :
+    #begin
+    x=1
+    x= x+3
+    x = 0
+    #end
+print(True or False)
+
+while (x<4):
+    #begin
+    x = x+1
+    #end
