13b1fb9ca6d69a97ddc3fe2d87940c966f58d52d
RobinSievic <sievicrobin@gmail.com>
2026-05-04 18:08:23 +0200

    Update evenodd.py

--- PythonToJS/test/evenodd.py
+++ PythonToJS/test/evenodd.py
@@ -10,38 +10,4 @@ def is_odd(n: int) -> bool:
     return((n != 0) and is_even(n - 1))
     #end
 
-def is_even(n: int) -> int:
-    #begin
-    y : int
-    y=3
-    n=2
-    n = n+1
-    if n==3:
-        #begin
-        return n-1
-        #end
-    else :
-        #begin
-        return n+4
-        #end
-    #end
 
-x : int
-x = 3
-if x==3:
-    #begin
-    x=4
-    x = 5
-    #end
-else :
-    #begin
-    x=1
-    x= x+3
-    x = 0
-    #end
-print(True or False)
-
-while (x<4):
-    #begin
-    x = x+1
-    #end
