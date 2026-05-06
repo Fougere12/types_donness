943d6826c058b8526af95a732effb7cc3276c22f
Fougere12 <ananasamendine@gmail.com>
2026-05-06 14:24:39 +0200

    Update and rename Verif_type_test_erreurs.py to erreur_variable_pas_instancie.py

--- PythonToJS/test/Verif_type_test_erreurs.py
+++
@@ -1,6 +0,0 @@
-def erreur_type_simple(x : int) -> None:            #erreur de type attendue
-  #begin
-  x : int
-  x = 3
-  x = "hello"
-  #end
---
+++ PythonToJS/test/erreur_variable_pas_instancie.py
@@ -0,0 +1,6 @@
+def erreur_type_simple(x : int) -> None:            #erreur retournee : variable pas instanciee
+  #begin
+  x : int
+  x = 3
+  x = "hello"
+  #end
